2c78d4d215a5fe3d478c3cdab6f9c11d073d82d6
DhruvRajpoot <dhruv.kutarya@gmail.com>
2023-02-15 21:40:44 +0530

    feat: Herotext

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,9 +6,8 @@ import Feature from './Feature'
 const images = [hero1, hero2]
 const Hero = () => (
     <>
-        <div style={{ background: '#2a2a2a', maxHeight: 'calc(15rem + 12vw)', zIndex: '1', paddingTop: '1rem',position:'relative'}}>
+        <div style={{ background: '#2a2a2a', maxHeight: 'calc(15rem + 12vw)', zIndex: '1', paddingTop: '1rem'}}>
             <Carousel images={images} width='87%' height='calc(18rem + 17vw)' />
-            
         </div>
         <Feature />
     </>
